8b99ade35ec430dffc960d18606a8cb73906c084
3Vis_N <francescotrevisan3@gmail.com>
2020-11-01 19:42:06 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+const int VUOTO   = 0;
+const int PLAYER1 = 1;
+const int PLAYER2 = 2;
+
+/* una stampa tutti vuoti e una seconda per assegnare i player */
+
+void inizializzazione(int *piano, int RIGHE, int COLONNE) {
+    int r, c;
+    for (r = 0; r < RIGHE; r++) {
+        for (c = 0; c < COLONNE; c++) {
+            if ((r % 2 == 0 && c % 2 == 0) || (r % 2 != 0 && c % 2 !=0)) { /*se righe e colonne sono entrambe pari o entrambe dispari stampa player1*/
+                if (r < 3) {
+                    piano[r * COLONNE + c] = PLAYER1;
+                } else if (r > 3) {
+                    piano[r * COLONNE + c] = PLAYER2;
+                } else {
+                    piano[r * COLONNE + c] = VUOTO;
+                }
+            }
+        }
+    }
+}
+
+
+
+void stampa(int *piano, int RIGA, int COLONNA) { /*RIGA/COLONNA=numero righe/colonne*/
+    int r=0, c;
+    for (r=0; r < RIGA; r++) { /*r=inidce riga, c=indice colonna*/
+        /*printf("%d", r); (r*COLONNA+c) TODO (DA FORMATTARE INDICI RIGA) */
+        for (c=0; c < COLONNA; c++) {
+            int casella = piano[r * COLONNA + c];
+            if (casella==PLAYER1) {
+                printf(" X ");
+            }
+            else if (casella==PLAYER2){
+                printf(" O ");
+            }
+            else {printf("   ");
+            }
+        }
+        printf("\n");
+    }
+    /*printf(" ");
+    for (c=0; c < COLONNA; c++)
+        printf("%d ", c);
+    printf("\n");*/
+}
+
+int main() {
+    int piano[7][7];
+    inizializzazione(&(piano[0][0]), 7, 7);
+    stampa(&(piano[0][0]), 7, 7);
+    return 0;
+}
